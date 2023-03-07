76f81b04c32428420746f9c5d5bd82c5a29a613b
Tyler Hatton <tyler.hatton@wwt.com>
2023-03-07 07:46:14 -0600

    add centos 8 commands

--- script.sh
+++ script.sh
@@ -1,9 +1,13 @@
-sudo yum install -y git docker python3-pip gcc openssl-devel bzip2-devel libffi-devel 
-sudo pip3 install docker-compose
+# centos 8 python install 3.6
+sudo dnf install python3
+
+
+sudo yum install -y git docker gcc openssl-devel bzip2-devel libffi-devel wget
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -a -G docker ec2-user
 
+# python 3.9 install steps
 cd /opt
 sudo wget https://www.python.org/ftp/python/3.9.16/Python-3.9.16.tgz 
 sudo tar xzf Python-3.9.16.tgz 
@@ -13,6 +17,9 @@ sudo make altinstall
 sudo rm -f /opt/Python-3.9.16.tgz
 
 sudo python3 -m pip install ansible
+sudo python3 -m pip install docker-compose
+
+
 sudo sysctl fs.inotify.max_user_watches=20000
 
 cd ~
