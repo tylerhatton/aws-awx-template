a3ad86258b55021d9b38e94c677bc87fd791841a
Tyler Hatton <tyler.hatton@wwt.com>
2023-03-07 07:46:57 -0600

    add centos 8 commands

--- script.sh
+++ script.sh
@@ -1,23 +1,27 @@
-# centos 8 python install 3.6
-sudo dnf install python3
 
-
-sudo yum install -y git docker gcc openssl-devel bzip2-devel libffi-devel wget
+sudo dnf install docker-ce --nobest --allowerasing -y
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo usermod -a -G docker ec2-user
+
+# create awx user
+sudo useradd -m -s /bin/bash awx
+
+# add user to docker group
+sudo usermod -a -G docker awx
 
 # python 3.9 install steps
-cd /opt
-sudo wget https://www.python.org/ftp/python/3.9.16/Python-3.9.16.tgz 
-sudo tar xzf Python-3.9.16.tgz 
-cd Python-3.9.16 
-sudo ./configure --enable-optimizations 
-sudo make altinstall 
-sudo rm -f /opt/Python-3.9.16.tgz
+# cd /opt
+# sudo wget https://www.python.org/ftp/python/3.9.16/Python-3.9.16.tgz 
+# sudo tar xzf Python-3.9.16.tgz 
+# cd Python-3.9.16 
+# sudo ./configure --enable-optimizations 
+# sudo make altinstall 
+# sudo rm -f /opt/Python-3.9.16.tgz
 
-sudo python3 -m pip install ansible
-sudo python3 -m pip install docker-compose
+sudo yum install python39
+
+sudo python3.9 -m pip install ansible
+sudo python3.9 -m pip install docker-compose
 
 
 sudo sysctl fs.inotify.max_user_watches=20000
@@ -26,3 +30,7 @@ cd ~
 git clone -b 21.11.0 https://github.com/ansible/awx.git
 cd awx
 # set secrets under tools/docker-compose/inventory
+make docker-compose-build
+make docker-compose
+docker exec tools_awx_1 make clean-ui ui-devel
+docker exec -ti tools_awx_1 awx-manage createsuperuser
